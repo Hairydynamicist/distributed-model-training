638cb4f4d29db0e71d2cba16cce50c6d924fb01f
John Murray <john.b.murray@gmail.com>
2024-05-20 10:31:49 +0100

    added MLFlowConfig line to InfrastructureConfig class in Infrastructure_configs

--- scripts/configs/infrastructure/infrastructure_configs.py
+++ scripts/configs/infrastructure/infrastructure_configs.py
@@ -5,9 +5,9 @@ from omegaconf import SI
 from scripts.configs.infrastructure.instance_group_creator_configs import InstanceGroupCreatorConfig
 #scripts.configs.infrastructure.instance_group_creator_configs
 @dataclass
-class MLFlowCOnfig:
-    mlflow_external_tracking_uri: str = SI("${oc.env:MLFLOW_TRACKING_URI, localhost:6101")
-    mlflow_internal_tracking_uri: str = SI("${oc.env:MLFLOW_INTERNAL_TRACKING_URI, localhost:6101")
+class MLFlowConfig:
+    mlflow_external_tracking_uri: str = SI("${oc.env:MLFLOW_TRACKING_URI, localhost:6101}")
+    mlflow_internal_tracking_uri: str = SI("${oc.env:MLFLOW_INTERNAL_TRACKING_URI, localhost:6101}")
     experiment_name: str = "Default"
     run_name: Optional[str] = None
     run_id: Optional[str] = None
@@ -23,3 +23,4 @@ class InfrastructureConfig:
     project_id: str = "cybulde"
     zone: str = "europe-west4-a"
     instance_group_creator: InstanceGroupCreatorConfig = field(default_factory=InstanceGroupCreatorConfig)
+    mlflow: MLFlowConfig = field(default_factory=MLFlowConfig)
